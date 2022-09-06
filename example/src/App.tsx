af731bad376dd15407a250483f09baf582d341ac
Ajit Kumar <ajitkumar@ajit-mackbook-pro.local>
2022-09-07 01:47:07 +0400

    package changes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { ExampleComponent } from 'decode-single-select'
 import 'decode-single-select/dist/index.css'
+import { Alert, Box, Divider } from '@chakra-ui/react'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  let dummy1 = {
+    title: 'How are you?',
+    options: [
+      { title: 'Good', id: 1 },
+      { title: 'happy', id: 2 },
+      { title: 'sad' }
+    ],
+    id: 1,
+    disabled: false,
+    random: false,
+    required: false,
+    type: 1
+  }
+  const [json] = useState(dummy1)
+  const [userRes, setUser] = useState({} as any)
+
+  const getJsonData = (data: any) => {
+    setUser(data)
+  }
+
+  return (
+    <div>
+      {JSON.stringify(json)}
+      <Divider />
+      {JSON.stringify(userRes)}
+      <Box>
+        <Divider h='10px' orientation='horizontal' />
+      </Box>
+      <Box p='10px'>
+        <Alert>Package</Alert>
+        <ExampleComponent json={json} getJsonData={getJsonData} />
+      </Box>
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react'
 import styles from './styles.module.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { SingleChoice } from './components/SingleChoice/SIngleChoice'
 
 interface Props {
-  text: string
+  json?: any
+  getJsonData?: any
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Random Text: {text}</div>
+export const ExampleComponent = ({ json, getJsonData }: Props) => {
+  return (
+    <ChakraProvider>
+      <div className={styles.test}>
+        {/* to write switch case for the types */}
+        <SingleChoice json={json} getJsonData={getJsonData}></SingleChoice>
+      </div>
+    </ChakraProvider>
+  )
 }
